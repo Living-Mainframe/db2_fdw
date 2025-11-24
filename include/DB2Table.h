384df38dc05d57056935f51876cc5efb52977627
Thomas Muenz <temuenz@t-online.de>
2025-11-24 11:16:08 +0100

    added batchsz as attribute of the table

--- include/DB2Table.h
+++ include/DB2Table.h
@@ -11,6 +11,7 @@
 typedef struct db2Table {
   char*               name;          // DB2 table name
   char*               pgname;        // PG tabelname, for error messages
+  int                 batchsz;       // Defined size of batch inserts
   int                 ncols;         // number of columns in DB2 table
   int                 npgcols;       // number of columns (including dropped) in the PostgreSQL foreign table
   DB2Column**         cols;          // pointer to an array of DB2Column descriptors, as many as ncols tells
