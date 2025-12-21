849c91ca9b29cae9cdf875fa700a3883c2d47e30
Thomas Muenz <temuenz@t-online.de>
2025-12-21 19:41:31 +0100

    enriching build workflow to sign rpms

--- docker/builder/scripts/sign_rpms.sh
+++ docker/builder/scripts/sign_rpms.sh
@@ -5,9 +5,8 @@ DEST=${DEST:-/host}
 cd "$DEST"
 export GPG_TTY=$(tty)
 
-sed "s/PASSPHRASE/${GPG_PASSPHRASE}/" config/rpmmacros >~/.rpmmacros
-echo "Importing pubkey..."
-gpg --import --no-tty --batch --yes <gpg_pubkey.asc
+sed "s/PASSPHRASE/${GPG_PASSPHRASE}/" config/signmacros >~/.rpmmacros
+gpg --import --no-tty --batch --yes <RPM-GPG-KEY-myrepo
 echo "Importing seckey..."
 echo "${GPG_KEY_B64}" | base64 -d | gpg --import --no-tty --batch --yes
 echo "rpmsign --addsign..."
