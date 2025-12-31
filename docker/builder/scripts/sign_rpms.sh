8cec52db395391d7af578a12bef9a2261ede5534
Thomas Muenz <temuenz@t-online.de>
2025-12-31 16:10:10 +0100

    check sign script

--- docker/builder/scripts/sign_rpms.sh
+++ docker/builder/scripts/sign_rpms.sh
@@ -5,7 +5,9 @@ DEST=${DEST:-/host}
 cd "$DEST"
 export GPG_TTY=$(tty)
 
+echo "PASSPHRASE: ${GPG_PASSPHRASE}"
 sed "s/PASSPHRASE/${GPG_PASSPHRASE}/" config/signmacros >~/.rpmmacros
+cat ~/.rpmmacros
 gpg --import --no-tty --batch --yes <RPM-GPG-KEY-myrepo
 echo "Importing seckey..."
 echo "${GPG_KEY_B64}" | base64 -d | gpg --import --no-tty --batch --yes
