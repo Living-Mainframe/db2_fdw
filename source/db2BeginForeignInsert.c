9e35a040a0072e0c96d17263485eb1f151883334
Thomas Muenz <temuenz@t-online.de>
2026-02-12 13:11:54 +0100

    cleanup the code formatting

--- source/db2BeginForeignInsert.c
+++ source/db2BeginForeignInsert.c
@@ -25,70 +25,59 @@ void                db2BeginForeignInsert      (ModifyTableState* mtstate, Resul
 DB2FdwState*        db2BuildInsertFdwState     (Relation rel);
 
 void db2BeginForeignInsert(ModifyTableState* mtstate, ResultRelInfo* rinfo) {
-    Relation     rel       = rinfo->ri_RelationDesc;
-    DB2FdwState *fdw_state = NULL;
-    db2Debug1("> db2BeginForeignInsert");
-    fdw_state = db2BuildInsertFdwState(rel);
-    /* subplan is irrelevant for pure INSERT/COPY */
-    db2BeginForeignModifyCommon(mtstate, rinfo, fdw_state, NULL);
-    db2Debug1("< db2BeginForeignInsert");
+  Relation     rel       = rinfo->ri_RelationDesc;
+  DB2FdwState* fdw_state = NULL;
+
+  db2Debug1("> db2BeginForeignInsert");
+  fdw_state = db2BuildInsertFdwState(rel);
+  /* subplan is irrelevant for pure INSERT/COPY */
+  db2BeginForeignModifyCommon(mtstate, rinfo, fdw_state, NULL);
+  db2Debug1("< db2BeginForeignInsert");
 }
 
 DB2FdwState* db2BuildInsertFdwState(Relation rel) {
-    DB2FdwState *fdwState;
-    StringInfoData sql;
-    int i;
-    bool firstcol;
-    db2Debug1("> db2BuildInsertFdwState");
+  DB2FdwState*    fdwState;
+  StringInfoData  sql;
+  int             i;
+  bool            firstcol;
 
-    /* Same logic as CMD_INSERT branch of db2PlanForeignModify: */
-    fdwState = db2GetFdwState(RelationGetRelid(rel), NULL, true);
-    initStringInfo(&sql);
-
-    appendStringInfo(&sql, "INSERT INTO %s (", fdwState->db2Table->name);
-    firstcol = true;
-    for (i = 0; i < fdwState->db2Table->ncols; ++i)
-    {
-        if (fdwState->db2Table->cols[i]->pgname == NULL)
-            continue;
-
-        if (!firstcol)
-            appendStringInfo(&sql, ", ");
-        else
-            firstcol = false;
-
-        appendStringInfo(&sql, "%s", fdwState->db2Table->cols[i]->colName);
+  db2Debug1("> db2BuildInsertFdwState");
+  /* Same logic as CMD_INSERT branch of db2PlanForeignModify: */
+  fdwState = db2GetFdwState(RelationGetRelid(rel), NULL, true);
+  initStringInfo(&sql);
+  appendStringInfo(&sql, "INSERT INTO %s (", fdwState->db2Table->name);
+  firstcol = true;
+  for (i = 0; i < fdwState->db2Table->ncols; ++i) {
+    if (fdwState->db2Table->cols[i]->pgname == NULL) {
+      continue;
     }
-    appendStringInfo(&sql, ") VALUES (");
-
-    firstcol = true;
-    for (i = 0; i < fdwState->db2Table->ncols; ++i)
-    {
-        if (fdwState->db2Table->cols[i]->pgname == NULL)
-            continue;
-
-        checkDataType(fdwState->db2Table->cols[i]->colType,
-                      fdwState->db2Table->cols[i]->colScale,
-                      fdwState->db2Table->cols[i]->pgtype,
-                      fdwState->db2Table->pgname,
-                      fdwState->db2Table->cols[i]->pgname);
-
-        addParam(&fdwState->paramList,
-                 fdwState->db2Table->cols[i]->pgtype,
-                 fdwState->db2Table->cols[i]->colType,
-                 i,
-                 0);
-
-        if (!firstcol)
-            appendStringInfo(&sql, ", ");
-        else
-            firstcol = false;
-
-        appendAsType(&sql, fdwState->db2Table->cols[i]->pgtype);
-    }
-    appendStringInfo(&sql, ")");
-    fdwState->query = sql.data;
-    db2Debug2("  fdwState->query: '%s'",sql.data);
-    db2Debug1("< db2BuildInsertFdwState - returns fdwState: %x",fdwState);
+    appendStringInfo(&sql, "%s%s", (firstcol) ? "" : ", ", fdwState->db2Table->cols[i]->colName);
+    firstcol = false;
+  }
+  appendStringInfo(&sql, ") VALUES (");
+  firstcol = true;
+  for (i = 0; i < fdwState->db2Table->ncols; ++i) {
+    if (fdwState->db2Table->cols[i]->pgname == NULL)
+      continue;
+    checkDataType(fdwState->db2Table->cols[i]->colType,
+                  fdwState->db2Table->cols[i]->colScale,
+                  fdwState->db2Table->cols[i]->pgtype,
+                  fdwState->db2Table->pgname,
+                  fdwState->db2Table->cols[i]->pgname);
+    addParam(&fdwState->paramList,
+             fdwState->db2Table->cols[i]->pgtype,
+             fdwState->db2Table->cols[i]->colType,
+             i,
+             0);
+    if (!firstcol)
+      appendStringInfo(&sql, ", ");
+    else
+      firstcol = false;
+    appendAsType(&sql, fdwState->db2Table->cols[i]->pgtype);
+  }
+  appendStringInfo(&sql, ")");
+  fdwState->query = sql.data;
+  db2Debug2("  fdwState->query: '%s'",sql.data);
+  db2Debug1("< db2BuildInsertFdwState - returns fdwState: %x",fdwState);
   return fdwState;
 }
