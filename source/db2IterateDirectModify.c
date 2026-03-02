6cc7c0567555c51a231473614ae120d25376409f
Thomas Muenz <temuenz@t-online.de>
2026-03-02 11:34:24 +0100

    added proper return of affected rows

--- source/db2IterateDirectModify.c
+++ source/db2IterateDirectModify.c
@@ -1,4 +1,6 @@
 #include <postgres.h>
+#include <catalog/pg_type.h>
+#include <utils/syscache.h>
 #include "db2_fdw.h"
 #include "DB2FdwDirectModifyState.h"
 
@@ -10,50 +12,53 @@ extern int          db2FetchNext              (DB2Session* session);
 extern void         db2CloseStatement         (DB2Session* session);
 
 /** local prototypes */
-TupleTableSlot* db2IterateDirectModify(ForeignScanState *node);
+       TupleTableSlot* db2IterateDirectModify(ForeignScanState *node);
 
-/* postgresIterateDirectModify
+       /* postgresIterateDirectModify
  * Execute a direct foreign table modification
  */
 TupleTableSlot* db2IterateDirectModify(ForeignScanState *node) {
   DB2FdwDirectModifyState* 	dmstate     = (DB2FdwDirectModifyState*) node->fdw_state;
-//  EState*                   estate      = node->ss.ps.state;
+  EState*                   estate      = node->ss.ps.state;
+  TupleTableSlot*           slot        = node->ss.ss_ScanTupleSlot;
 //  ResultRelInfo*            rtinfo      = node->resultRelInfo;
   int                       have_result = 0;
-  TupleTableSlot*           slot        = NULL;
+  MemoryContext             oldcontext;
 
   // nachfolgende Werte in ParamDesc Liste zusammenfassen
-  //int             numParams     = dmstate->numParams;
-  //const char**    values        = dmstate->param_values;
-  //FmgrInfo*       param_flinfo  = dmstate->param_flinfo;
-  //List*           param_exps    = dmstate->param_exprs;
+//  int             numParams     = dmstate->numParams;
+//  const char**    values        = dmstate->param_values;
+//  FmgrInfo*       param_flinfo  = dmstate->param_flinfo;
+//  List*           param_exps    = dmstate->param_exprs;
   
   db2Entry1();
   // We should have a valid session.
   // It was created during db2GetFdwDirectModifyState during db2BeginDirectModify
+  MemoryContextReset (dmstate->temp_cxt);
+  oldcontext = MemoryContextSwitchTo (dmstate->temp_cxt);
+  db2PrepareQuery(dmstate->session, dmstate->query, NULL, dmstate->prefetch, dmstate->fetch_size);
+  dmstate->num_tuples = db2ExecuteQuery (dmstate->session, dmstate->paramList);
+  db2Debug2(" %d rows affected by this query", have_result);
+  MemoryContextSwitchTo (oldcontext);
 
-  if (db2IsStatementOpen(dmstate->session)) {
+  slot = ExecClearTuple (slot);
+  if (dmstate->num_tuples) {
+    Instrumentation*  instr = node->ss.ps.instrument;
 
+    /* Increment the command es_processed count if necessary. */
+    if (dmstate->set_processed)
+      estate->es_processed += dmstate->num_tuples;
+
+    /* Increment the tuple count for EXPLAIN ANALYZE if necessary. */
+    if (instr)
+      instr->tuplecount += dmstate->num_tuples;
+
+    /* initialize virtual tuple */
   } else {
-    db2PrepareQuery(dmstate->session, dmstate->query, NULL, dmstate->prefetch, dmstate->fetch_size);
-    have_result = db2ExecuteQuery (dmstate->session, dmstate->paramList);
-    db2Debug2(" %d rows affected by this query", have_result);
-  }
-
-  /* initialize virtual tuple */
-//  ExecClearTuple (slot);
-//  if (have_result) {
-    /* increase row count */
-//    ++dmstate->rowcount;
-    /* convert result to arrays of values and null indicators */
-//    db2Debug2("slot->tts_tupleDescriptor->natts: %d",slot->tts_tupleDescriptor->natts);
-//    convertTuple (dmstate, slot->tts_tupleDescriptor->natts, slot->tts_values, slot->tts_isnull, false);
-    /* store the virtual tuple */
-//    ExecStoreVirtualTuple (slot);
-//  } else {
     /* close the statement */
     db2CloseStatement (dmstate->session);
-//  }
+  }
+
   db2Exit1(": %x", slot);
   return slot;
 }
